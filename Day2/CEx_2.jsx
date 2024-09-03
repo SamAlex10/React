3b18e32f23d5138ae5f2b7b946a26e111ec24b08
SamAlex10 <24eucy501@skcet.ac.in>
2024-09-03 10:48:02 +0530

    Create CEx_2.jsx

---
+++ Day2/CEx_2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Wel = () =>
+{
+  return (
+    <div>
+    <h1 style={{color:'green'}}>Inline Style in JSX Example.</h1>
+    <div style={{backgroundColor:'darkblue', color:'darkblue', padding:'10px',border:'1px solid blue',borderRadius:'5px'}}>
+      <p style={{color:'darkblue',fontSize:'16px'}}>This is a paragraph with inline styles applied.</p>
+    </div>
+    </div>
+  )
+}
+export default Wel;
